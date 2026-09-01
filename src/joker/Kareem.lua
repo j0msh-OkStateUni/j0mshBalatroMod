be6abca6d26831301ddf87cf2841986277f9368e
j0msh <joshuapatel84@gmail.com>
2026-09-01 07:28:07 -0500

    Made my placeholder cards and I'm about to make my first joker

---
+++ src/joker/Kareem.lua
@@ -0,0 +1,13 @@
+SMODS.Joker{
+    key = 'Kareem',
+    atlas = 'placeholder',
+    pos = {x = 0, y = 0},
+    config = {
+
+    },
+    rarity = 1,
+    cost = 3,
+    loc_vars = function(self, info_queue, card)
+
+    end
+}
