43058053d20c5a77ba6e50bfdd30e123f0800d53
j0msh <joshuapatel84@gmail.com>
2026-09-03 03:41:47 -0500

    moved all jokers into their respective era folders and updated main so it reads the joker code no matter how deep the subfolder its in

--- main.lua
+++ main.lua
@@ -9,17 +9,32 @@ SMODS.Atlas{
 
 
 
-
-
-local jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/joker")
-
 SMODS.current_mod.optional_features = function()
     return {
         post_trigger = true
     }
 end
 
-for _, file in ipairs(jokers_src) do
-    assert(SMODS.load_file("src/joker/" .. file))()
+
+local function load_joker_folder(directory)
+    local items = SMODS.NFS.getDirectoryItems(
+        SMODS.current_mod.path .. directory
+    )
+
+    for _, item in ipairs(items) do
+        local relative_path = directory .. "/" .. item
+        local absolute_path =
+            SMODS.current_mod.path .. relative_path
+
+        local info = SMODS.NFS.getInfo(absolute_path)
+
+        if info and info.type == "directory" then
+            load_joker_folder(relative_path)
+
+        elseif item:lower():sub(-4) == ".lua" then
+            assert(SMODS.load_file(relative_path))()
+        end
+    end
 end
 
+load_joker_folder("src/joker")
